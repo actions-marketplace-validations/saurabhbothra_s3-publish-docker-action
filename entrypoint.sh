ab64096f596336b93bf7fe3550ca2f92c1d70be1
Saurabh Bothra <sabothra@amazon.com>
2022-03-08 14:46:33 -0800

    changes to workflow for testing

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+# performing validations.
 if [ -z "$S3_BUCKET_NAME" ]; then
   echo "S3_BUCKET_NAME is not set. Quitting."
   exit 1
@@ -37,21 +38,6 @@ if [ -z "$AWS_SESSION_TOKEN" ]; then
   exit 1
 fi
 
-# mkdir -p ~/.aws
-# touch ~/.aws/config
-# touch ~/.aws/credentials
-
-# echo "[profile s3-publish-action]
-# region = ${AWS_REGION}
-# role = ${AWS_ROLE}
-# account = ${AWS_ACCOUNT_ID}" > ~/.aws/config
-
-# echo "[s3-publish-action]
-# aws_access_key_id = ${AWS_ACCESS_KEY_ID}
-# aws_secret_access_key = ${AWS_SECRET_ACCESS_KEY}
-# aws_session_token = ${AWS_SESSION_TOKEN}" > ~/.aws/credentials
-
-
+# s3 sync command to copy directory in local filesystem to an s3 bucket.
 sh -c "aws s3 sync ${SOURCE_DIR:-.} s3://${S3_BUCKET_NAME}/${TARGET_DIR} \
               $*"
-# rm -rf ~/.aws
